2177dc1622da632aa7e601eabb94e50c0927f2d5
Carlos Ochoa <uligibson@hotmail.com>
2023-07-05 16:48:23 -0600

    ButtonProps updated

--- src/components/Button/types.d.ts
+++ src/components/Button/types.d.ts
@@ -6,7 +6,7 @@ export type ButtonProps = {
   size?: 'sm' | 'md' | 'lg'
   color?: '' | 'default' | 'primary' | 'secondary' | 'danger'
   children?: React.ReactNode
-}
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 export type StyledButtonProps = {
   $variant?: 'default' | 'outline' | 'text'
